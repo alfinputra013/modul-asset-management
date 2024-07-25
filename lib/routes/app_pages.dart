a93b6b7df6592957e1e68699671661cbb6fd7bd8
Yogapratama1109 <yoga110011@gmail.com>
2024-07-25 09:35:38 +0700

    add route

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -1,5 +1,7 @@
 import 'package:get/get.dart';
 import 'package:modul_asset_management/app/auth/loginpage.dart';
+import 'package:modul_asset_management/app/views/detail-item/all_data_assets.dart';
+import 'package:modul_asset_management/app/views/detail-item/all_data_stock.dart';
 import 'package:modul_asset_management/app/views/detail-item/detail_item.dart';
 import 'package:modul_asset_management/app/views/detail-item/detail_item_opname.dart';
 import 'package:modul_asset_management/app/views/homepage.dart';
@@ -9,21 +11,29 @@ import '../main.dart';
 
 part 'app_routes.dart';
 
-class  AppPages {
-AppPages._();
+class AppPages {
+  AppPages._();
 
-static const INITIAL = Routes.LOGIN;
+  static const INITIAL = Routes.LOGIN;
 
-static final routes = [
-  GetPage(
+  static final routes = [
+    GetPage(
       name: _Paths.HOME,
       page: () => const HomePage(),
     ),
-     GetPage(
+    GetPage(
+      name: _Paths.ALLDATA,
+      page: () => const AllDataAssets(),
+    ),
+    GetPage(
+      name: _Paths.ALLDATASTOCK,
+      page: () => const AllDataStock(),
+    ),
+    GetPage(
       name: _Paths.LOGIN,
       page: () => const LoginPage(),
     ),
-     GetPage(
+    GetPage(
       name: _Paths.EDITNOTES,
       page: () => const EditNotes(),
     ),
@@ -31,9 +41,9 @@ static final routes = [
       name: _Paths.DETAIL,
       page: () => const DetailItem(),
     ),
-     GetPage(
+    GetPage(
       name: _Paths.DETAILOPNAME,
       page: () => const DetailItemOpname(),
     ),
-];
+  ];
 }
--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -3,6 +3,8 @@ part of 'app_pages.dart';
 abstract class Routes {
   Routes._();
   static const HOME = _Paths.HOME;
+  static const ALLDATASTOCK = _Paths.ALLDATASTOCK;
+  static const ALLDATA = _Paths.ALLDATA;
   static const LOGIN = _Paths.LOGIN;
   static const EDITNOTES = _Paths.EDITNOTES;
   static const DETAILOPNAME = _Paths.DETAILOPNAME;
@@ -11,6 +13,8 @@ abstract class Routes {
 
 abstract class _Paths {
   static const HOME = '/home';
+  static const ALLDATASTOCK = '/alldatastock';
+  static const ALLDATA = '/alldata';
   static const EDITNOTES = '/editnotes';
   static const LOGIN = '/login';
   static const DETAIL = '/detail';
