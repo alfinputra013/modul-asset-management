01fa16216b200d0e6ebde2ffd776d0a44c47a182
Yogapratama1109 <yoga110011@gmail.com>
2024-07-25 09:36:54 +0700

    update tabbar

--- lib/app/views/detail-item/detail_item_opname.dart
+++ lib/app/views/detail-item/detail_item_opname.dart
@@ -5,6 +5,7 @@ import 'package:modul_asset_management/app/views/tab-bar-view.dart/assets_detail
 import 'package:modul_asset_management/app/views/tab-bar-view.dart/notes_all.dart';
 import 'package:modul_asset_management/app/views/tab-bar-view.dart/stock_opname.dart';
 import 'package:modul_asset_management/core/theme.dart';
+import 'package:modul_asset_management/widget/app_bar.dart';
 
 class DetailItemOpname extends StatefulWidget {
   const DetailItemOpname({super.key});
@@ -28,72 +29,45 @@ class _DetailItemOpnameState extends State<DetailItemOpname>
     super.dispose();
   }
 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: whiteColor,
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        automaticallyImplyLeading: false,
-        title: GestureDetector(
-          onTap: () {
-            Get.back();
-          },
-          child: Row(
-            children: [
-              Icon(
-                Icons.arrow_back_ios_new,
-                color: greyFont,
-              ),
-              const SizedBox(
-                width: 3,
-              ),
-              Text(
-                "Back",
-                style: GoogleFonts.poppins(
-                  fontSize: 18,
-                ),
-              ),
-            ],
+      appBar: AppBarGlobal(
+        title: "Stock Opname",
+        centerTitle: false,
+        backgroundColor: orangeMain,
+        leading: GestureDetector(
+          onTap: () => Get.back(),
+          child: Icon(
+            Icons.arrow_back_ios_new,
+            color: whiteColor,
+            size: 20,
           ),
         ),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 20),
-            child: Text(
-              "Stock Opname",
-              style: GoogleFonts.poppins(
-                fontSize: 16,
-                fontWeight: FontWeight.w500,
-                color: orangeMain,
-              ),
-            ),
-          ),
-        ],
       ),
       body: Column(
         children: [
           Padding(
             padding: const EdgeInsets.only(left: 20, right: 20, top: 10),
             child: Container(
-              // transform: Matrix4.translationValues(0, -40, 0),
               height: 35,
               decoration: BoxDecoration(
                 color: Colors.white,
-                border: Border.all(
-                  color: greyFont,
-                  width: 1.3,
-                ),
                 borderRadius: BorderRadius.circular(5),
               ),
               child: TabBar(
                 dividerHeight: 0,
                 controller: _tabController,
-                indicatorSize: TabBarIndicatorSize.tab,
-                indicator: BoxDecoration(
-                  borderRadius: BorderRadius.circular(2),
-                  color: Colors.blueAccent,
+                indicatorSize: TabBarIndicatorSize.label,
+                indicator: UnderlineTabIndicator(
+                  borderSide: BorderSide(color: orangeMain, width: 2.5),
+                  insets: const EdgeInsets.symmetric(horizontal: 5.0),
+                  borderRadius: const BorderRadius.vertical(
+                    top: Radius.circular(5),
+                  ),
                 ),
-                labelColor: Colors.white,
+                labelColor: orangeMain,
                 unselectedLabelColor: greyFont,
                 labelStyle:
                     const TextStyle(fontSize: 11, fontWeight: FontWeight.w500),
