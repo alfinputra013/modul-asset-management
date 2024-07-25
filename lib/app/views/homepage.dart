fb35dba318f453ce645da681cd5c6a5a467ad109
Yogapratama1109 <yoga110011@gmail.com>
2024-07-25 09:37:11 +0700

    update landing page

--- lib/app/views/homepage.dart
+++ lib/app/views/homepage.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:modul_asset_management/app/views/detail-item/detail_item.dart';
 import 'package:modul_asset_management/core/theme.dart';
 import 'package:modul_asset_management/routes/app_pages.dart';
+import 'package:modul_asset_management/widget/app_bar.dart';
 import 'package:modul_asset_management/widget/list_data.dart';
 import 'package:qrscan/qrscan.dart' as scanner;
 
@@ -15,7 +15,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-    Future<void> scanQR(BuildContext context) async {
+  Future<void> scanQR(BuildContext context) async {
     String scanResult;
     try {
       scanResult = (await scanner.scan())!;
@@ -39,141 +39,154 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+      backgroundColor: whiteColor,
+      appBar: AppBarGlobal(
+        title: 'Modul Asset Management',
+        centerTitle: true,
         automaticallyImplyLeading: false,
         backgroundColor: orangeMain,
-        centerTitle: true,
-        title: Text(
-          "Asset Management & Inventory",
-          style: GoogleFonts.poppins(
-            color: Colors.white,
-            fontWeight: FontWeight.w500,
-            fontSize: 14,
-          ),
-        ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 24),
+              child: Column(
                 children: [
-                  Text(
-                    "Asset Maintenance",
-                    style: GoogleFonts.poppins(
-                      fontSize: 16,
-                    ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Asset Maintenance",
+                        style: GoogleFonts.poppins(
+                          fontSize: 16,
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {
+                          Get.toNamed(Routes.ALLDATA);
+                        },
+                        child: Text(
+                          "See All",
+                          style: GoogleFonts.poppins(
+                            fontSize: 13,
+                            color: orangeMain,
+                            decoration: TextDecoration.underline,
+                            decorationColor: orangeMain,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                  Text(
-                    "See All",
-                    style: GoogleFonts.poppins(
-                      fontSize: 15,
-                      color: Colors.blue[900],
-                      decoration: TextDecoration.underline,
-                    ),
+                  const SizedBox(
+                    height: 10,
                   ),
+                  ListView.builder(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: 3,
+                    itemBuilder: (context, index) {
+                      return InkWell(
+                        onTap: () {
+                          Get.toNamed(Routes.DETAIL);
+                        },
+                        child: const CustomListItem(
+                          maintenance: "Cleansing Hardisk",
+                          subtitle1: "Notebokk A",
+                          subtitle2: "Laptop",
+                          date: "12-12-2022",
+                        ),
+                      );
+                    },
+                  ),
+                  const SizedBox(
+                    height: 26,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Stock Opname Due",
+                        style: GoogleFonts.poppins(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {
+                          Get.toNamed(Routes.ALLDATASTOCK);
+                        },
+                        child: Text(
+                          "See All",
+                          style: GoogleFonts.poppins(
+                            fontSize: 13,
+                            color: orangeMain,
+                            decoration: TextDecoration.underline,
+                            decorationColor: orangeMain,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  ListView.builder(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: 4,
+                    itemBuilder: (context, index) {
+                      return InkWell(
+                        onTap: () {
+                          Get.toNamed(Routes.DETAILOPNAME);
+                        },
+                        child: const CustomListItem(
+                          maintenance: "Cleansing Hardisk",
+                          subtitle1: "Notebokk A",
+                          subtitle2: "Laptop",
+                          date: "12-12-2022",
+                        ),
+                      );
+                    },
+                  ),
+                  SizedBox(height: 30,),
                 ],
               ),
-              const SizedBox(
-                height: 10,
-              ),
-              Container(
-                height: 300,
-                decoration: BoxDecoration(border: Border.all()),
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  itemCount: 3,
-                  itemBuilder: (context, index) {
-                    return InkWell(
-                      onTap:  () {
-                       Get.toNamed(Routes.DETAIL);
-                      },
-                      child: const CustomListItem(
-                        maintenance: "Cleansing Hardisk",
-                        subtitle1: "Notebokk A",
-                        subtitle2: "Laptop",
-                        date: "12-12-2022",
-                      ),
-                    );
-                  },
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Stock Opname Due",
-                    style: GoogleFonts.poppins(
-                      fontSize: 16,
-                    ),
-                  ),
-                  Text(
-                    "See All",
-                    style: GoogleFonts.poppins(
-                      fontSize: 15,
-                      color: Colors.blue[900],
-                      decoration: TextDecoration.underline,
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Container(
-                height: 200,
-                decoration: BoxDecoration(border: Border.all()),
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  itemCount: 2,
-                  itemBuilder: (context, index) {
-                    return InkWell(
-                      onTap: () {
-                       Get.toNamed(Routes.DETAILOPNAME);
-                      },
-                      child: const CustomListItem(
-                        maintenance: "Cleansing Hardisk",
-                        subtitle1: "Notebokk A",
-                        subtitle2: "Laptop",
-                        date: "12-12-2022",
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
       floatingActionButton: Padding(
-        padding: const EdgeInsets.only(bottom: 20),
-        child: FloatingActionButton(
-          backgroundColor: whiteColor,
-          onPressed: () {
-          scanQR(context);
-          },
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(28.0),
-          ),
-          child: const Icon(
-            Icons.qr_code_2,
-            size: 34,
+        padding: const EdgeInsets.only(bottom: 8, right: 8),
+        child: SizedBox(
+          height: 60,
+          width: 60,
+          child: FloatingActionButton(
+            elevation: 0,
+            backgroundColor: orangeMain,
+            onPressed: () {
+              scanQR(context);
+            },
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(100.0),
+            ),
+            child: SizedBox(
+              height: 60,
+              width: 60,
+              child: const Icon(
+                Icons.qr_code_2,
+                size: 34,
+                color: Colors.white,
+              ),
+            ),
           ),
         ),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      // floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
-
-
